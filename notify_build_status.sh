c97153c3ac36263fc91c915fc64cdb029cbeca22
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-03-28 11:01:36 +0100

    fix: mark build as failure on any other code than 0

--- notify_build_status.sh
+++ notify_build_status.sh
@@ -8,9 +8,10 @@ function notify_build_status() {
   
   case "$1" in
     0) result='SUCCESS' ;;
-    1) result='FAILURE' ;;
+    *) result='FAILURE' ;;
   esac
   echo $3
   notify-send "Build $result [$2s]" "$3" -i ~/.icons/ike_build_${result:l}.png -t 15000
+  return $1
 }
 
