eba6a44999ca705443ea237d889ca448e3c1c4c8
Bartosz Majsak <bartosz.majsak@gmail.com>
2026-02-13 08:27:56 +0100

    feat(nvim): add Go run keymaps
    Add <leader>rr to run go project and <leader>rf to run current file
    in a terminal split.
    
    Signed-off-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Add any additional keymaps here
+
+-- Go: run current project
+vim.keymap.set("n", "<leader>rr", function()
+  vim.cmd("split | terminal go run .")
+end, { desc = "Go: run project" })
+
+-- Go: run current file
+vim.keymap.set("n", "<leader>rf", function()
+  vim.cmd("split | terminal go run " .. vim.fn.expand("%"))
+end, { desc = "Go: run current file" })
